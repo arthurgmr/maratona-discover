4289c76988ee91b032188fa4a4151c6aa24740f6
arthurgmr <arthur.gmr@hotmail.com>
2021-04-05 23:23:18 -0300

    aula2

--- src/server.js
+++ src/server.js
@@ -6,8 +6,9 @@ server.set('view engine', 'ejs');
 
 server.use(express.static("public"))
 
-server.use(routes)
+server.use(express.urlencoded({ extended:true }))
 
+server.use(routes)
 
 
 server.listen(3000, () => console.log('server running'));
